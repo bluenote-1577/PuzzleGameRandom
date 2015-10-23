35746f9af2b2fda6185449bf8a137ec710c5d130
Jim Shaw <jimshawster@gmail.com>
2015-10-22 19:17:09 -0700

    Almost finiished twitter analysis, retweets to go

--- src/twitterAnalysis/TwitterAnalysis.java
+++ src/twitterAnalysis/TwitterAnalysis.java
@@ -17,16 +17,16 @@ import ca.ubc.ece.cpen221.mp3.graph.*;
 
 public class TwitterAnalysis {
 
-	public static void main(String[] args) throws IOException{
-		//store all the queries into the map. 
-		//if the users have been queries already,
-		//we can check if it's a duplicate.
-		
-		Map allQueries = new HashMap<String,Boolean>();
+	public static void main(String[] args) throws IOException {
+		// store all the queries into the map.
+		// if the users have been queries already,
+		// we can check if it's a duplicate.
+
+		Map<String, Boolean> allQueries = new HashMap<String, Boolean>();
 		Graph myGraph = new AdjacencyMatrixGraph();
-		
+
 		twitterScan("datasets/test1.txt", myGraph);
-		
+
 		File file = new File("datasets/outTest.txt");
 
 		// if file doesnt exists, then create it
@@ -35,78 +35,84 @@ public class TwitterAnalysis {
 		}
 		Scanner queryScan = new Scanner(new BufferedReader(new FileReader("datasets/test1.txt")));
 		BufferedWriter bw = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
-		
-		
-		try{
+
+		try {
 			queryScan = new Scanner(new BufferedReader(new FileReader("datasets/query.txt")));
-			
-			while(queryScan.hasNext()){
+
+			while (queryScan.hasNext()) {
 				String queryType = queryScan.next();
 				String user1 = queryScan.next();
 				String user2 = queryScan.next();
 				String question = queryScan.next();
-				
-				if (!question.equals("?") || allQueries.containsKey(user1+user2+queryType));
-				
-				else{
-					if (queryType.equals("commonInfluencers")){
-						commonInfluencers(user1,user2,bw);
+
+				if (!question.equals("?") || allQueries.containsKey(user1 + user2 + queryType))
+					;
+
+				else {
+					if (queryType.equals("commonInfluencers")) {
+						commonInfluencers(user1, user2, bw, myGraph);
+
 					}
-					
-					if (queryType.equals("numRetweets")){
-						numRetweets(user1,user2,bw);
+
+					if (queryType.equals("numRetweets")) {
+						numRetweets(user1, user2, bw, myGraph);
 					}
-					
+					allQueries.put((user1 + user2 + queryType), true);
+
 				}
-				
-				
 			}
-		}
-		finally{
-			if (queryScan !=null){
+		} finally {
+			if (queryScan != null) {
 				queryScan.close();
 			}
+			if (bw != null){
+				bw.close();
+			}
 		}
-		
+
 	}
-		
-		
-	
-	private static void commonInfluencers(String user1, String user2, BufferedWriter bw){
-		
+
+	private static void commonInfluencers(String user1, String user2, BufferedWriter bw, Graph graph)
+			throws IOException {
+
+		bw.write("query: commonInfluencers " + user1 + " " + user2 + "\n");
+		bw.write("<result>\n");
+		for (Vertex vertex : Algorithms.commonDownstreamVertices(graph, new Vertex(user1), new Vertex(user2))) {
+			bw.write(vertex.toString() + "\n");
+		}
+		bw.write("</result>\n");
 	}
-	
-	private static void numRetweets(String user1, String user2, BufferedWriter bw){
-		
+
+	private static void numRetweets(String user1, String user2, BufferedWriter bw, Graph graph) {
+
 	}
-	
-	private static void twitterScan (String filename, Graph graph) throws FileNotFoundException{
+
+	private static void twitterScan(String filename, Graph graph) throws FileNotFoundException {
 		Scanner twitterscan = null;
-		try{
+		try {
 			twitterscan = new Scanner(new BufferedReader(new FileReader(filename)));
-			
-			while(twitterscan.hasNext()){
+
+			while (twitterscan.hasNext()) {
 				String followerName = twitterscan.next();
 				System.out.print(followerName);
 				twitterscan.next();
 				String followedName = twitterscan.next();
 				System.out.print(followedName);
 				System.out.println("");
-				
+
 				Vertex follower = new Vertex(followerName);
 				Vertex followed = new Vertex(followedName);
-				
+
 				graph.addVertex(follower);
 				graph.addVertex(followed);
 				graph.addEdge(follower, followed);
-				
+
 			}
-		}
-		finally{
-			if (twitterscan !=null){
+		} finally {
+			if (twitterscan != null) {
 				twitterscan.close();
 			}
 		}
-		
+
 	}
 }
